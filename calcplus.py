2676287a9389b4f78f0bb289dbed720c6aa74e9e
irenerl10 <irene17a@gmail.com>
2018-09-24 15:50:14 +0200

    Prueba

---
+++ calcplus.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+
+import sys
+from calcoohija  import CalculadoraHija
+from calcoo import Calculadora
+
+fich = open("fichero.csv", "r")
+
+for line in fich:
+	
+
