c8f0a189986109aebbabeb80b670c6991e42fefb
irenerl10 <irene17a@gmail.com>
2018-09-26 13:45:33 +0200

    me falta bucle para operar

--- calcplus.py
+++ calcplus.py
@@ -2,11 +2,32 @@
 # -*- coding: utf-8 -*-
 
 import sys
-from calcoohija  import CalculadoraHija
 from calcoo import Calculadora
+from calcoohija import CalculadoraHija
 
-fich = open("fichero.csv", "r")
+infile= open(sys.argv[1], 'r')
 
-for line in fich:
+operadores = []
+operandos = []
+
+for line in infile:
+	operadores.insert(0,line[:line.find(",")])
+	operandos.insert(0,line[:line.find(' ')].split(',')[1:])
+print(operadores)
+print('xxxxxxxxxxxxxxxxxxxxxxxxxx')
+print(operandos)
+
+if __name__ == "__main__":
+	try:
+		suma = operadores[0]
+		resta = operadores[1]
+		multiplica = operadores[2]
+		divide = operadores[3]
+	except ValueError:
+		sys.exit("Error: Non numerical parameters")
+
+	
+
+	
 	
 
