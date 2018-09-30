c2af34fcfd6f9be0b815cfac6d1aec92b7ac7f0b
irenerl10 <irene17a@gmail.com>
2018-09-30 22:35:06 +0200

    Terminada

--- calcoohija.py
+++ calcoohija.py
@@ -5,33 +5,35 @@ import sys
 from calcoo import Calculadora
 
 class CalculadoraHija:
-	def multiply(op1, op2):
-		"Function to multiply the operants"
-		return op1 * op2
 
-	def division(op1, op2):
-		"Function to division the operants"
+	def __init__(self, op1, op2):
+		self.op1 = op1
+		self.op2 = op2
+
+	def multiply(op1, op2):
+		return self.op1 * self.op2
+
+	def division(self):
 		try:
-			return op1 / op2
+			return self.op1 / self.op2
 		except ZeroDivisionError:
 			print('Division by zero is not allowed')
+			#necesito quitar que result no me devuelva none
 
 if __name__ == "__main__":
 	try:
-		operando1 = int(sys.argv[1])
-		operando2 = int(sys.argv[3])
+		operacion = CalculadoraHija(int(sys.argv[1]), int(sys.argv[3]))
 	except ValueError:
 		sys.exit("Error: Non numerical parameters")
 
 	if sys.argv[2] == "multiplica":
-		result = CalculadoraHija.multiply(operando1, operando2)
+		result = operacion.multiply()
 	elif sys.argv[2] == "divide":
-		result = CalculadoraHija.division(operando1, operando2)
-	#Quiero saber si esto puedo implementar directamente la calculadora sin tener que repetir codigo
+		result = operacion.division()
 	elif sys.argv[2] == "suma":
-		result = Calculadora.plus(operando1, operando2)
+		result = operacion.plus()
 	elif sys.argv[2] == "resta":
-		result = Calculadora.minus(operando1, operando2)
+		result = operacion.minus()
 	print(result)
 	
 
