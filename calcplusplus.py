eb6d5654063267316d21955c6841b6b0ebc07387
irenerl10 <irene17a@gmail.com>
2018-10-01 00:00:55 +0200

    No me opera con el primer operando

--- calcplusplus.py
+++ calcplusplus.py
@@ -3,8 +3,27 @@
 
 import csv
 import sys
+from calcoo import Calculadora
+from calcoohija import CalculadoraHija
 
 with open(sys.argv[1], newline='') as csv_archivo:
 	entrada = csv.reader(csv_archivo)
 	for line in entrada:
-		print(line)
+		operador = line[0]
+		operandos = line[1:]
+		result = int(operandos[1])
+		for op in operandos[2:]:
+			#no se porque no me lee el primer operando
+			#las operaciones se hacen bien pero falta el primero
+			#acordarse de mirar calcoohija en division 0 none
+			if operador == 'suma':
+				result = Calculadora(result, int(op)).plus()
+			elif operador == 'resta':
+				result = Calculadora(result, int(op)).minus()
+			elif operador == 'multiplica':
+				result = CalculadoraHija(result, int(op)).multiply()
+			elif operador == 'divide':
+				result = CalculadoraHija(result, int(op)).division()
+
+		print(result)
+
