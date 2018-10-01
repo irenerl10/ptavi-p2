768738a64fd9ee85ae02b403e3d22b05dff519a7
Irene Rodriguez Lopez <irolo@gamma39.aulas.gsyc.urjc.es>
2018-10-01 10:52:18 +0200

    TERMINADO

--- calcplusplus.py
+++ calcplusplus.py
@@ -7,19 +7,19 @@ from calcoo import Calculadora
 from calcoohija import CalculadoraHija
 
 with open(sys.argv[1], newline='') as csv_archivo:
-	entrada = csv.reader(csv_archivo)
-	for line in entrada:
-		operador = line[0]
-		operandos = line[1:]
-		result = int(operandos[0])
-		for op in operandos[1:]:
-			if operador == 'suma':
-				result = Calculadora(result, int(op)).plus()
-			elif operador == 'resta':
-				result = Calculadora(result, int(op)).minus()
-			elif operador == 'multiplica':
-				result = CalculadoraHija(result, int(op)).multiply()
-			elif operador == 'divide':
-				result = CalculadoraHija(result, int(op)).division()
+    entrada = csv.reader(csv_archivo)
+    for line in entrada:
+        operador = line[0]
+        operandos = line[1:]
+        result = int(operandos[0])
+        for op in operandos[1:]:
+            if operador == 'suma':
+                result = Calculadora(result, int(op)).plus()
+            elif operador == 'resta':
+                result = Calculadora(result, int(op)).minus()
+            elif operador == 'multiplica':
+                result = CalculadoraHija(result, int(op)).multiply()
+            elif operador == 'divide':
+                result = CalculadoraHija(result, int(op)).division()
 
-		print(result)
+        print(result)
