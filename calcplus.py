2b7485bb94a4b7bacb8878b16cc44225b1f3324c
Irene Rodriguez Lopez <irolo@gamma39.aulas.gsyc.urjc.es>
2018-10-01 10:52:15 +0200

    TERMINADO

--- calcplus.py
+++ calcplus.py
@@ -5,21 +5,21 @@ import sys
 from calcoo import Calculadora
 from calcoohija import CalculadoraHija
 
-infile= open(sys.argv[1], 'r')
+infile = open(sys.argv[1], 'r')
 
 
 for line in infile:
-	operador = line.split(',')[0]
-	operandos = line.split(',')[1:]
-	result = int(operandos[0])
-	for op in operandos[1:]:
-		if operador == 'suma':
-			result = Calculadora(result, int(op)).plus()
-		elif operador == 'resta':
-			result = Calculadora(result, int(op)).minus()
-		elif operador == 'multiplica':
-			result = CalculadoraHija(result, int(op)).multiply()
-		elif operador == 'divide':
-			result = CalculadoraHija(result, int(op)).division()
+    operador = line.split(',')[0]
+    operandos = line.split(',')[1:]
+    result = int(operandos[0])
+    for op in operandos[1:]:
+        if operador == 'suma':
+            result = Calculadora(result, int(op)).plus()
+        elif operador == 'resta':
+            result = Calculadora(result, int(op)).minus()
+        elif operador == 'multiplica':
+            result = CalculadoraHija(result, int(op)).multiply()
+        elif operador == 'divide':
+            result = CalculadoraHija(result, int(op)).division()
 
-	print(result)	
+    print(result)
