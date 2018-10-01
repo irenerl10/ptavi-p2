9f2e4544579d182db112c5079026fae437a118ef
irenerl10 <irene17a@gmail.com>
2018-10-01 10:30:27 +0200

    Terminado falta pep8

--- calcplus.py
+++ calcplus.py
@@ -11,11 +11,8 @@ infile= open(sys.argv[1], 'r')
 for line in infile:
 	operador = line.split(',')[0]
 	operandos = line.split(',')[1:]
-	result = int(operandos[1])
-	for op in operandos[2:]:
-		#no se porque no me lee el primer operando
-		#las operaciones se hacen bien pero falta el primero
-		#acordarse de mirar calcoohija en division 0 none
+	result = int(operandos[0])
+	for op in operandos[1:]:
 		if operador == 'suma':
 			result = Calculadora(result, int(op)).plus()
 		elif operador == 'resta':
@@ -25,6 +22,4 @@ for line in infile:
 		elif operador == 'divide':
 			result = CalculadoraHija(result, int(op)).division()
 
-	print(result)
-
-	
+	print(result)	
