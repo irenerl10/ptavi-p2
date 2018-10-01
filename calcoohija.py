bfb73097d1654f37a221d0fc4005aa7ef9b05564
Irene Rodriguez Lopez <irolo@gamma39.aulas.gsyc.urjc.es>
2018-10-01 10:52:09 +0200

    TERMINADO

--- calcoohija.py
+++ calcoohija.py
@@ -4,35 +4,35 @@
 import sys
 from calcoo import Calculadora
 
+
 class CalculadoraHija:
 
-	def __init__(self, op1, op2):
-		self.op1 = op1
-		self.op2 = op2
+    def __init__(self, op1, op2):
+        self.op1 = op1
+        self.op2 = op2
 
-	def multiply(self):
-		return self.op1 * self.op2
+    def multiply(self):
+        return self.op1 * self.op2
 
-	def division(self):
-		try:
-			return self.op1 / self.op2
-		except ZeroDivisionError:
-			return 'Division by zero is not allowed'
+    def division(self):
+        try:
+            return self.op1 / self.op2
+        except ZeroDivisionError:
+            return 'Division by zero is not allowed'
+
 
 if __name__ == "__main__":
-	try:
-		operacion = CalculadoraHija(int(sys.argv[1]), int(sys.argv[3]))
-	except ValueError:
-		sys.exit("Error: Non numerical parameters")
+    try:
+        operacion = CalculadoraHija(int(sys.argv[1]), int(sys.argv[3]))
+    except ValueError:
+        sys.exit("Error: Non numerical parameters")
 
-	if sys.argv[2] == "multiplica":
-		result = operacion.multiply()
-	elif sys.argv[2] == "divide":
-		result = operacion.division()
-	elif sys.argv[2] == "suma":
-		result = operacion.plus()
-	elif sys.argv[2] == "resta":
-		result = operacion.minus()
-	print(result)
-	
-
+    if sys.argv[2] == "multiplica":
+        result = operacion.multiply()
+    elif sys.argv[2] == "divide":
+        result = operacion.division()
+    elif sys.argv[2] == "suma":
+        result = operacion.plus()
+    elif sys.argv[2] == "resta":
+        result = operacion.minus()
+    print(result)
